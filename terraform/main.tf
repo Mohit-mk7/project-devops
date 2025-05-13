4c1241ddedcf286d586ac74b49b7a4c1c8a564c5
Mohit-mk7 <ghostmohit77@gmail.com>
2025-05-13 16:41:05 +0530

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,30 @@ module "ecr" {
   ecr_name   = var.ecr_name
 }
 
+
+
+
+
+resource "null_resource" "generate_inventory_and_copy_key" {
+  provisioner "local-exec" {
+    command = <<EOT
+# Write inventory.ini
+echo "[jump]
+jump ansible_host=${module.ec2.jump_server_public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ac190-mohit/Documents/my-ec2-key.pem
+
+[private]
+private ansible_host=${module.ec2.private_server_private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=/home/ac190-mohit/Documents/my-ec2-key.pem ansible_ssh_common_args='-o ProxyCommand=\"ssh -i /home/ac190-mohit/Documents/my-ec2-key.pem -W %h:%p ubuntu@${module.ec2.jump_server_public_ip}\"'
+" > /home/ac190-mohit/Documents/ansibleterraform/inventory.ini
+
+# Copy SSH key to jump server
+scp -o StrictHostKeyChecking=no -i /home/ac190-mohit/Documents/my-ec2-key.pem /home/ac190-mohit/Documents/my-ec2-key.pem ubuntu@${module.ec2.jump_server_public_ip}:~/my-ec2-key.pem
+EOT
+  }
+
+  triggers = {
+    jump_ip    = module.ec2.jump_server_public_ip
+    private_ip = module.ec2.private_server_private_ip
+  }
+
+  depends_on = [module.ec2]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,8 @@ output "jump_server_public_ip" {
 output "private_server_private_ip" {
   value = module.ec2.private_server_private_ip
 }
+
+
+
+
+
